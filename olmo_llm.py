6f487bd2e2d57a968e73777e00639367927f82ac
aman-17 <amanrangapur@gmail.com>
2024-10-21 17:45:48 -0500

    update

--- olmo_llm.py
+++ olmo_llm.py
@@ -1,8 +1,10 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+"""
+Intermediate checkpoints missing for OLMo-1B. LINK: https://huggingface.co/allenai/OLMo-1B/discussions/11
+"""
+from hf_olmo import OLMoForCausalLM, OLMoTokenizerFast
 
-
-olmo = AutoModelForCausalLM.from_pretrained("allenai/OLMo-1B-hf")
-tokenizer = AutoTokenizer.from_pretrained("allenai/OLMo-1B-hf")
+olmo = OLMoForCausalLM.from_pretrained("allenai/OLMo-1B", revision="step733000-tokens3074B")
+tokenizer = OLMoTokenizerFast.from_pretrained("allenai/OLMo-1B")
 message = ["Language modeling is"]
 inputs = tokenizer(message, return_tensors='pt', return_token_type_ids=False)
 response = olmo.generate(**inputs, max_new_tokens=100, do_sample=True, top_k=50, top_p=0.95)

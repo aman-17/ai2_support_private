8592c93e45c10c8c35af4b86d15ababe31b79b87
Aman Rangapur <amanrangapur@gmail.com>
2024-11-18 17:11:31 -0600

    Create move_unshard.sh

---
+++ release/move_unshard.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SOURCE_DIR="/data/input/ai2-llm/checkpoints/OLMo-medium/peteish7"
+DEST_DIR="/data/input/amanr/safetens_unshard"
+
+mkdir -p "$DEST_DIR"
+
+steps=($(ls "$SOURCE_DIR" | grep -Eo 'step[0-9]+-unsharded' | grep -Eo '[0-9]+' | sort -n | awk '$1 >= 0 && $1 <= 248000'))
+total=${#steps[@]}
+
+echo "Found $total unsharded checkpoints to move"
+echo "Moving checkpoints..."
+
+for ((i=0; i<${#steps[@]}; i++)); do
+    step=${steps[i]}
+    current=$((i + 1))
+    percentage=$((current * 100 / total))
+
+    printf "\rProgress: [%-50s] %d%% (%d/%d)" \
+        $(printf "#%.0s" $(seq 1 $((percentage/2)))) \
+        $percentage $current $total
+
+    mv "$SOURCE_DIR/step${step}-unsharded" "$DEST_DIR/"
+done
+
+echo -e "\nMove completed!"
